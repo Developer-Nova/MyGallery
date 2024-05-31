32ed12a2b526b16ede6d2c10abc188f4dd411d03
Nova <woohyeon3382@naver.com>
2024-05-31 20:50:27 +0900

    docs: Deployments 설정 및 gitignore 파일 설정

---
+++ MyGallery/MyGallery/MyGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGalleryApp.swift
+//  MyGallery
+//
+//  Created by Nova on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct MyGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
