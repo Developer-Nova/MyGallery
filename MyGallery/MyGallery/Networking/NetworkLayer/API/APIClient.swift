e7bdd43e33c6e73a7a086825dd129952e3f085f6
Nova <woohyeon3382@naver.com>
2024-06-02 21:51:48 +0900

    feat: sendRequest 메서드 구현

--- MyGallery/MyGallery/Networking/NetworkLayer/API/APIClient.swift
+++ MyGallery/MyGallery/Networking/NetworkLayer/API/APIClient.swift
@@ -5,8 +5,6 @@
 //  Created by Nova on 6/2/24.
 //
 
-import Foundation
-
 protocol APIClient {
     func sendRequest<T: Decodable>(endpoint: String, method: HTTPMethod, completion: @escaping (Result<T, APIError>) -> Void)
 }
--- MyGallery/MyGallery/Networking/NetworkLayer/API/UnSplashAPIClient.swift
+++ MyGallery/MyGallery/Networking/NetworkLayer/API/UnSplashAPIClient.swift
@@ -17,6 +17,38 @@ final class UnsplashAPIClient: APIClient {
     }
     
     func sendRequest<T>(endpoint: String, method: HTTPMethod, completion: @escaping (Result<T, APIError>) -> Void) where T : Decodable {
+        guard let url = URL(string: endpoint, relativeTo: baseURL) else {
+            completion(.failure(.invalidURL))
+            return
+        }
         
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(.requestFailed))
+                return
+            }
+            
+            guard let response = response else { return }
+            
+            // todo: 각 response 처리 필요
+            
+            guard let data = data else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let decodedData = try decoder.decode(T.self, from: data)
+                completion(.success(decodedData))
+            } catch {
+                completion(.failure(.decodingFailed))
+            }
+        }
+        
+        task.resume()
     }
 }
