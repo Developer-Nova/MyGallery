ad7688ee525a1e988117127f2cb89a7e8665442c
Nova <woohyeon3382@naver.com>
2024-06-05 22:01:36 +0900

    feat: URLError 타입에 errorDescription 구현
    LocalizedError 채택

--- MyGallery/MyGallery/Network/Error/URLError.swift
+++ MyGallery/MyGallery/Network/Error/URLError.swift
@@ -11,3 +11,14 @@ enum URLError: Error {
     case URLcomponentsError
     case isEmpty
 }
+
+extension URLError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .URLcomponentsError:
+            "URL 구성 요소를 생성하는데 실패했습니다."
+        case .isEmpty:
+            "URL 이 비어 있습니다."
+        }
+    }
+}
