69cf4d308258d970aa01a8ed02c68a9679418bbf
Nova <woohyeon3382@naver.com>
2024-06-05 21:25:00 +0900

    refactor: 그룹화 수정

---
+++ MyGallery/MyGallery/Network/DTO/PhotoRequestDTO.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoRequestDTO.swift
+//  MyGallery
+//
+//  Created by Nova on 6/4/24.
+//
+
+struct PhotoRequestDTO: Encodable { }
--- MyGallery/MyGallery/Network/DTO/PhotoResponseDTO.swift
+++ MyGallery/MyGallery/Network/DTO/PhotoResponseDTO.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-// MARK: - PhotoObject
+// MARK: - PhotoResponseDTO
 struct PhotoResponseDTO: Decodable {
     let id: String
     let alternativeSlugs: AlternativeSlug
