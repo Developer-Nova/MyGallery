bfb984472fa904db74a2e5ef23d8e8b6f3288bbb
Nova <woohyeon3382@naver.com>
2024-06-08 03:24:24 +0900

    feat: CollectionView 생성

---
+++ MyGallery/MyGallery/Feature/Home/Collection/CollectionView.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionView.swift
+//  MyGallery
+//
+//  Created by Nova on 6/8/24.
+//
+
+import SwiftUI
+
+struct CollectionView: View {
+    var body: some View {
+        Text("CollectionView")
+    }
+}
+
+#Preview {
+    CollectionView()
+}
