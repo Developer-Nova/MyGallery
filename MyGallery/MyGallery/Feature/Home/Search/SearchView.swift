552986af844a1f6b6118a4269afc747c2a7100f5
Nova <woohyeon3382@naver.com>
2024-06-08 01:05:16 +0900

    feat: SearchView 및 SearchViewModel 정의

---
+++ MyGallery/MyGallery/Feature/Home/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  MyGallery
+//
+//  Created by Nova on 6/8/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("SearchView")
+    }
+}
+
+#Preview {
+    SearchView()
+}
