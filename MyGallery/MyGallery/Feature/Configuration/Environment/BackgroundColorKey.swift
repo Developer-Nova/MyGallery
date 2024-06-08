806af53458e077c3eeaa7b899160016db690b800
Nova <woohyeon3382@naver.com>
2024-06-09 02:43:24 +0900

    feat: EnvironmentKey 설정

---
+++ MyGallery/MyGallery/Feature/Configuration/Environment/BackgroundColorKey.swift
@@ -0,0 +1,12 @@
+//
+//  BackgroundColorKey.swift
+//  MyGallery
+//
+//  Created by Nova on 6/9/24.
+//
+
+import SwiftUI
+
+struct BackgroundColorKey: EnvironmentKey {
+    static let defaultValue: Color = .white
+}
