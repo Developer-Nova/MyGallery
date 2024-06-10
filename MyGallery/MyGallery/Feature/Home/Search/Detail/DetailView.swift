5e15d177d300f54d31f8c196ebae64872834b4f1
Nova <woohyeon3382@naver.com>
2024-06-10 23:42:12 +0900

    feat: SearchView 의 DetailView 생성

---
+++ MyGallery/MyGallery/Feature/Home/Search/Detail/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  MyGallery
+//
+//  Created by Nova on 6/10/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
