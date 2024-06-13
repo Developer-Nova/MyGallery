eb228b5c1fce52af29ed12c43b246ffec6ec4cc3
Nova <woohyeon3382@naver.com>
2024-06-13 18:56:04 +0900

    refactor: CustomTabBar 적용
    기존의 .tabItem 을 사용하지 않고 custom 한 CustomTabBar 를 ZStack 으로 적용

--- MyGallery/MyGallery/Feature/Home/HomeView.swift
+++ MyGallery/MyGallery/Feature/Home/HomeView.swift
@@ -11,33 +11,27 @@ struct HomeView: View {
     @StateObject private var pathModel = Path()
     @StateObject private var homeViewModel = HomeViewModel()
     
-    init() {
-        UITabBar.appearance().backgroundColor = UIColor.customBlack1
-    }
-    
     var body: some View {
         NavigationStack(path: $pathModel.paths) {
-            TabView(selection: $homeViewModel.selectedTab) {
-                SearchView()
-                    .tabItem {
-                        Image(
-                            homeViewModel.selectedTab == .search
-                            ? "search_selected"
-                            : "search"
-                        )
-                    }
-                    .tag(Tab.search)
+            ZStack {
+                TabView(selection: $homeViewModel.selectedTab) {
+                    Group {
+                        SearchView()
+                            .tag(Tab.search)
+                        
+                        CollectionView()
+                            .tag(Tab.collection)
+                    } //: Group
+                    .toolbar(.hidden, for: .tabBar)
+                } //: TabView
                 
-                CollectionView()
-                    .tabItem {
-                        Image(
-                            homeViewModel.selectedTab == .collection
-                            ? "collection_selected"
-                            : "collection"
-                        )
-                    }
-                    .tag(Tab.collection)
-            } //: TabView
+                VStack {
+                    Spacer()
+                    
+                    CustomTabBar(homeViewModel: homeViewModel)
+                        .padding(.bottom, 5)
+                } //: VStack
+            } //: ZStack
         } //: NavigationStack
     }
 }
