8ad1acc8d6e480199e5472cae5ca8f02c238c6ec
Nova <woohyeon3382@naver.com>
2024-06-15 01:23:46 +0900

    feat: SearchViewModel 구현

--- MyGallery/MyGallery/Feature/Home/Search/SearchViewModel.swift
+++ MyGallery/MyGallery/Feature/Home/Search/SearchViewModel.swift
@@ -5,8 +5,28 @@
 //  Created by Nova on 6/8/24.
 //
 
-import Foundation
+import SwiftUI
 
 final class SearchViewModel: ObservableObject {
+    @Published var photoList = [PhotoResponseDTO]()
+    private let networkProvider = NetworkProvider()
     
+    var columns: [GridItem] {
+        Array(repeating: .init(.flexible(), spacing: 3), count: 3)
+    }
+    
+    func getNewPhotoList() {
+        let endpoint = UnsplashAPIEndpoints.getPhotoListEndpoint(query: PhotoRequestDTO(), path: UnsplashAPI.Path.photos)
+        
+        networkProvider.request(endpoint: endpoint) { result in
+            switch result {
+            case .success(let photo):
+                DispatchQueue.main.async {
+                    self.photoList = photo
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
