39507f8f5c91647c98e1f890a759be981e0f03f7
Nova <woohyeon3382@naver.com>
2024-06-15 01:17:07 +0900

    Revert "feat: PhotoRequestDTO 구현"
    This reverts commit 364cb949e073e74aa92e7c20e8b4cc312641068d.

--- MyGallery/MyGallery/Network/Provider/Providable.swift
+++ MyGallery/MyGallery/Network/Provider/Providable.swift
@@ -7,5 +7,46 @@
 import Foundation
 
 protocol Providable {
+    var session: URLSession { get set }
+    
     func request<R: Decodable, E: RequestResponsable>(endpoint: E, completion: @escaping (Result<R, NetworkError>) -> Void) where E.Response == R
+}
+
+extension Providable {
+    func request<R: Decodable, E: RequestResponsable>(endpoint: E, completion: @escaping (Result<R, NetworkError>) -> Void) where E.Response == R  {
+        guard let urlRequest = try? endpoint.createURLRequest() else {
+            completion(.failure(.requestFailed))
+            return
+        }
+        
+        session.dataTask(with: urlRequest) { data, response, error in
+            guard error == nil else {
+                completion(.failure(.requestFailed))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.unknown))
+                return
+            }
+            
+            guard (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(.badResponse(statusCode: httpResponse.statusCode)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.emptyData))
+                return
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode(R.self, from: data)
+                completion(.success(decodedData))
+            } catch {
+                completion(.failure(NetworkError.decodingFailed))
+            }
+            
+        }.resume()
+    }
 }
