63979e4d4a1ccf21b6cb25304738c02119d63bbe
Nova <woohyeon3382@naver.com>
2024-06-15 01:21:03 +0900

    feat: PhotoRequestDTO 구현

--- MyGallery/MyGallery/Network/DTO/PhotoRequestDTO.swift
+++ MyGallery/MyGallery/Network/DTO/PhotoRequestDTO.swift
@@ -5,4 +5,20 @@
 //  Created by Nova on 6/4/24.
 //
 
-struct PhotoRequestDTO: Encodable { }
+struct PhotoRequestDTO: Encodable { 
+    let page: Int = 1
+    var perPage: Int = 30
+    var orderBy: OrderBy = .popular
+    
+    private enum CodingKeys: String, CodingKey {
+        case page
+        case perPage = "per_page"
+        case orderBy = "order_by"
+    }
+}
+
+enum OrderBy: String, Encodable {
+    case latest
+    case oldest
+    case popular
+}
