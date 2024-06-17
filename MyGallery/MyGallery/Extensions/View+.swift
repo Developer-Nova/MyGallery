2019298f2ee6be4bb0741da4694e45d422ce2ae8
Nova <woohyeon3382@naver.com>
2024-06-18 00:40:22 +0900

    feat: hideKeyboard 메서드 구현

--- MyGallery/MyGallery/Extensions/View+.swift
+++ MyGallery/MyGallery/Extensions/View+.swift
@@ -11,4 +11,8 @@ extension View {
     func applyBackgroundColor() -> some View {
         self.modifier(BackgroundColorModifier())
     }
+    
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 }
