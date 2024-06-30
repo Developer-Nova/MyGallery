26de51889ee1cf2299d6c39f284e2b09fc8fb3ce
Nova <woohyeon3382@naver.com>
2024-06-30 21:04:42 +0900

    refactor: DetailView 네이밍 변경
    DetailView -> PhotoDescriptionView
    DetailViewModel -> PhotoDescriptionViewModel

---
+++ MyGallery/MyGallery/Feature/Home/PhotoDescription/PhotoDescriptionView.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoDescriptionView.swift
+//  MyGallery
+//
+//  Created by Nova on 6/10/24.
+//
+
+import SwiftUI
+
+struct PhotoDescriptionView: View {
+    private var photo: Photo
+    
+    init(photo: Photo) {
+        self.photo = photo
+    }
+    
+    var body: some View {
+        Image(uiImage: photo.image)
+    }
+}
+
+#Preview {
+    PhotoDescriptionView(photo: Photo(image: UIImage()))
+}
