11c5b945f0b0ddc457f1e97439cf582f4a5d69ee
Nova <woohyeon3382@naver.com>
2024-07-02 21:07:18 +0900

    feat: SearchViewModel 메서드 구현
    - Combine 적용
    - getNewPhotoList 메서드 구현
    - getSearchPhotoList 메서드 구현
    - conversionImage 메서드 구현

--- MyGallery/MyGallery/Feature/Home/Search/SearchViewModel.swift
+++ MyGallery/MyGallery/Feature/Home/Search/SearchViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 final class SearchViewModel: ObservableObject {
     @Published var photoList = [Photo]()
@@ -14,23 +15,89 @@ final class SearchViewModel: ObservableObject {
     @Published private(set) var isEmptyImage = false
     
     private let networkProvider = NetworkProvider()
+    private var cancellables = Set<AnyCancellable>()
     
     var columns: [GridItem] {
         Array(repeating: .init(.flexible(), spacing: 3), count: 3)
     }
     
     func getNewPhotoList() {
-        let endpoint = UnsplashAPIEndpoints.getPhotoListEndpoint(query: PhotoRequestDTO(), path: UnsplashAPI.Path.photos)
+        let requestDTO = NewPhotoListRequestDTO()
+        let endpoint = UnsplashAPIEndpoints.getPhotoListEndpoint(query: requestDTO, path: UnsplashAPI.Path.photos, type: [PhotoResponseDTO].self)
         
-        networkProvider.request(endpoint: endpoint) { result in
-            switch result {
-            case .success(let photo):
-                DispatchQueue.main.async {
-                    self.photoList = photo
-                }
-            case .failure(let error):
-                print(error)
+        self.isLoading = true
+        self.networkProvider.request(endpoint: endpoint)
+            .flatMap { photos in
+                Publishers.MergeMany(photos.map { self.conversionImage(with: $0.urls.small) })
+                    .collect()
             }
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    self.isLoading = false
+                case .failure(let error):
+                    print(error)
+                    self.isLoading = false
+                }
+            } receiveValue: { images in
+                self.photoList = images.map { Photo(image: $0) }
+            }
+            .store(in: &cancellables)
+    }
+    
+    func getSearchPhotoList() {
+        let requestDTO = SearchPhotoListRequestDTO(query: searchText)
+        let endpoint = UnsplashAPIEndpoints.getPhotoListEndpoint(query: requestDTO, path: UnsplashAPI.Path.search, type: SearchResultResponseDTO.self)
+        
+        self.isLoading = true
+        self.networkProvider.request(endpoint: endpoint)
+            .receive(on: DispatchQueue.main)
+            .flatMap { searchResult -> AnyPublisher<[UIImage], NetworkError> in
+                if searchResult.results.isEmpty {
+                    self.isEmptyImage = true
+                    return Just([])
+                        .setFailureType(to: NetworkError.self)
+                        .eraseToAnyPublisher()
+                }
+                
+                self.isEmptyImage = false
+                
+                return Publishers.MergeMany(searchResult.results.map { self.conversionImage(with: $0.urls.small)})
+                    .collect()
+                    .eraseToAnyPublisher()
+            }
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    self.isLoading = false
+                case .failure(let error):
+                    print(error)
+                    self.isLoading = false
+                }
+            } receiveValue: { images in
+                self.photoList = images.map { Photo(image: $0) }
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func conversionImage(with urlString: String) -> AnyPublisher<UIImage, NetworkError> {
+        guard let url = URL(string: urlString) else {
+            return Fail(error: NetworkError.invalidResponse).eraseToAnyPublisher()
         }
+     
+        return networkProvider.request(url: url)
+            .tryMap { data in
+                guard let image = UIImage(data: data) else {
+                    throw NetworkError.invalidResponse
+                }
+                
+                return image
+            }
+            .mapError { _ in
+                NetworkError.unknown
+            }
+            .eraseToAnyPublisher()
     }
 }
