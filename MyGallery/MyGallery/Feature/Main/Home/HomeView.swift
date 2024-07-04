063bfb00afa6691e2d2671fe2485844ff08c99e1
Nova <woohyeon3382@naver.com>
2024-07-04 23:56:10 +0900

    feat: HomView 와 HomeViewModel 생성

---
+++ MyGallery/MyGallery/Feature/Main/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MyGallery
+//
+//  Created by Nova on 7/4/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ MyGallery/MyGallery/Feature/Main/Home/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  MyGallery
+//
+//  Created by Nova on 7/4/24.
+//
+
+import Foundation
+
+final class HomeViewModel: ObservableObject {
+    
+}
