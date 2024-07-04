3385c8fa7c28df301adee7228e3175c23ad92d14
Nova <woohyeon3382@naver.com>
2024-07-04 21:13:21 +0900

    refactor: SearchViewModel 수정
    - init 생성자 구현 및 초기화
    - isInitialAppear 선언
    - morePhotoList 메서드 구현
    - changeInitialAppear 메서드 구현
    - isLoading 프로퍼티 Bool 타입 변경 부분 .toggle() 메서드로 변경

--- MyGallery/MyGallery/Feature/Home/Search/SearchViewModel.swift
+++ MyGallery/MyGallery/Feature/Home/Search/SearchViewModel.swift
@@ -9,63 +9,99 @@ import SwiftUI
 import Combine
 
 final class SearchViewModel: ObservableObject {
-    @Published var photoList = [Photo]()
-    @Published var searchText = ""
-    @Published private(set) var isLoading = false
-    @Published private(set) var isEmptyImage = false
+    @Published var photoList: [Photo]
+    @Published var searchText: String
+    @Published private(set) var isLoading: Bool
+    @Published private(set) var isInitialAppear: Bool
     
+    private(set) var selection: Selection
+    private var currentPage: Int
+    private var cancellables: Set<AnyCancellable>
     private let networkService = NetworkService.shared
-    private var cancellables = Set<AnyCancellable>()
+   
     
     var columns: [GridItem] {
         Array(repeating: .init(.flexible(), spacing: 3), count: 3)
     }
     
+    init(
+        photoList: [Photo] = [Photo](),
+        searchText: String = "",
+        isLoading: Bool = false,
+        isInitialAppear: Bool = true,
+        selection: Selection = Selection.newPhoto,
+        currentPage: Int = 1,
+        cancellables: Set<AnyCancellable> = Set<AnyCancellable>()
+    ) {
+        self.photoList = photoList
+        self.searchText = searchText
+        self.isLoading = isLoading
+        self.isInitialAppear = isInitialAppear
+        self.selection = selection
+        self.currentPage = currentPage
+        self.cancellables = cancellables
+    }
+}
+
+extension SearchViewModel {
+    func changeInitialAppear() {
+        self.isInitialAppear.toggle()
+    }
+    
     func clearSearchBarAndLoadImages() {
         self.searchText = ""
-        self.isEmptyImage = false
+        self.currentPage = 1
+        self.photoList.removeAll()
+        self.getNewPhotoList()
+    }
+    
+    func morePhotoList(of selection: Selection) {
+        self.currentPage += 1
+        
+        switch selection {
+        case .newPhoto:
+            getNewPhotoList()
+        case .searchPhoto:
+            getSearchPhotoList()
+        }
     }
     
     func getNewPhotoList() {
         self.isLoading = true
+        self.selection = .newPhoto
         
-        networkService.getNewPhotoList()
+        networkService.fetchNewPhotoList(page: currentPage)
             .receive(on: DispatchQueue.main)
             .sink { completion in
                 switch completion {
                 case .finished:
-                    self.isLoading = false
+                    self.isLoading.toggle()
                 case .failure(let error):
                     print(error)
-                    self.isLoading = false
+                    self.isLoading.toggle()
                 }
             } receiveValue: { images in
-                self.photoList = images.map { Photo(image: $0) }
+                self.photoList.append(contentsOf: images.map { Photo(image: $0) })
             }
             .store(in: &cancellables)
     }
     
     func getSearchPhotoList() {
         self.isLoading = true
+        self.selection = .searchPhoto
         
-        networkService.getSearchPhotoList(about: searchText)
+        networkService.fetchSearchPhotoList(about: searchText, page: currentPage)
             .receive(on: DispatchQueue.main)
             .sink { completion in
                 switch completion {
                 case .finished:
-                    self.isLoading = false
+                    self.isLoading.toggle()
                 case .failure(let error):
                     print(error)
-                    self.isLoading = false
+                    self.isLoading.toggle()
                 }
             } receiveValue: { images in
-                guard !images.isEmpty else {
-                    self.isEmptyImage = true
-                    return
-                }
-                
-                self.isEmptyImage = false
-                self.photoList = images.map { Photo(image: $0) } // Todo - photo 객체 수정하기
+                self.photoList.append(contentsOf: images.map { Photo(image: $0) }) // Todo - photo 객체 수정하기
             }
             .store(in: &cancellables)
     }
