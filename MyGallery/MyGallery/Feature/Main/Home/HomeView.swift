8954633320dc1f5cd08ed998b82486e263fced65
Nova <woohyeon3382@naver.com>
2024-07-06 20:52:56 +0900

    feat: TitleView 구현

--- MyGallery/MyGallery/Feature/Main/Home/HomeView.swift
+++ MyGallery/MyGallery/Feature/Main/Home/HomeView.swift
@@ -18,6 +18,46 @@ struct HomeView: View {
     }
 }
 
+// MARK: - TitleView
+private struct TitleView: View {
+    @EnvironmentObject private var pathModel: Path
+    
+    fileprivate init() { }
+    
+    fileprivate var body: some View {
+        VStack {
+            HStack {
+                Text("My")
+                    .strikethrough()
+                
+                Text("Gallery")
+                
+                Spacer()
+                
+                Button (action: {
+                    pathModel.paths.append(.searchPhotoView)
+                },
+                 label: {
+                    Image("search")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                    
+                }) //: Button
+                .padding(.trailing, 7)
+            } //: HStack
+            .font(.title)
+            .fontWeight(.bold)
+            .foregroundStyle(Color.white)
+            .padding(.horizontal)
+            
+            Rectangle()
+                .frame(height: 2)
+                .foregroundStyle(Color.gray.opacity(0.2))
+        } //: VStack
+        .padding(.top, 10)
+    }
+}
+
 #Preview {
     HomeView()
         .applyBackgroundColor()
