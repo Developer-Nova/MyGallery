8a2f00128fd21627c6818e4596e9871350a64fc4
Nova <woohyeon3382@naver.com>
2024-07-15 20:30:17 +0900

    refactor: NoImagesView 제거
    - 파일분리

--- MyGallery/MyGallery/Feature/Main/Search/SearchView.swift
+++ MyGallery/MyGallery/Feature/Main/Search/SearchView.swift
@@ -153,32 +153,7 @@ private struct PhotoScrollView: View {
     }
 }
 
-// MARK: - NoImagesView
-private struct NoImagesView: View {
-    
-    fileprivate init() { }
-    
-    fileprivate var body: some View {
-        VStack {
-            Spacer()
-            
-            Image("no_Image")
-            
-            Text("No Image")
-                .font(.title)
-                .fontWeight(.bold)
-                .padding(.horizontal)
-            
-            Text("해당 검색어로 이미지를 찾을 수 없습니다.\n다른 검색어를 시도해 주세요.")
-                .font(.callout)
-                .fontWeight(.semibold)
-                .multilineTextAlignment(.center)
-                .padding(.top, 5)
-                .padding(.horizontal)
-            
-            Spacer()
-        } //: VStack
-        .foregroundStyle(Color.gray)
+        .frame(maxWidth: 380)
     }
 }
 
@@ -186,10 +161,4 @@ private struct NoImagesView: View {
     SearchView()
         .environment(\.backgroundColor, .customBlack0)
         .environmentObject(Path())
-}
-
-#Preview("NoImagesView") {
-    NoImagesView()
-        .applyBackgroundColor()
-        .environment(\.backgroundColor, .customBlack0)
 }
