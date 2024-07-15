3cd5700bca25e5ca9dc16f75b569124d9329014d
Nova <woohyeon3382@naver.com>
2024-07-15 20:24:57 +0900

    feat: RecentSearchView
    - 최근 검색어를 보여주는 view

---
+++ MyGallery/MyGallery/Feature/Main/Search/RecentSearch/RecentSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  RecentSearchView.swift
+//  MyGallery
+//
+//  Created by Nova on 7/14/24.
+//
+
+import SwiftUI
+
+struct RecentSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentSearchView()
+}
