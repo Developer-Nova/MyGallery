c2713ac42afded592b0c40ad084409eeeb9c999a
Nova <woohyeon3382@naver.com>
2024-08-07 19:40:42 +0900

    refactor: Urls 타입 이름 변경
    Urls -> PhotoUrls

--- MyGallery/MyGallery/Network/DTO/ResponseDTO/PhotoResponseDTO.swift
+++ MyGallery/MyGallery/Network/DTO/ResponseDTO/PhotoResponseDTO.swift
@@ -16,7 +16,7 @@ struct PhotoResponseDTO: ResponseDTO {
     let blurHash: String?
     let description: String?
     let altDescription: String?
-    let urls: Urls
+    let photoUrls: PhotoUrls
     let links: Links
     let likes: Int
     let assetType: AssetType
@@ -36,110 +36,144 @@ struct PhotoResponseDTO: ResponseDTO {
         case blurHash = "blur_hash"
         case description
         case altDescription = "alt_description"
-        case urls
+        case photoUrls = "urls"
         case links
         case likes
         case assetType = "asset_type"
         case user
     }
+}
+
+// MARK: - AlternativeSlug
+struct AlternativeSlug: ResponseDTO {
+    let ko: String
+}
+
+// MARK: - Urls
+struct PhotoUrls: ResponseDTO {
+    let raw: String
+    let full: String
+    let regular: String
+    let small: String
+    let thumb: String
+    let smallS3: String
     
-    
-    // MARK: - AlternativeSlug
-    struct AlternativeSlug: ResponseDTO {
-        let ko: String
+    private enum CodingKeys: String, CodingKey {
+        case raw
+        case full
+        case regular
+        case small
+        case thumb
+        case smallS3 = "small_s3"
     }
+}
+
+// MARK: - Links
+struct Links: ResponseDTO {
+    let downloadLocation: String
     
-    // MARK: - Urls
-    struct Urls: ResponseDTO {
-        let raw: String
-        let full: String
-        let regular: String
-        let small: String
-        let thumb: String
-        let smallS3: String
-        
-        private enum CodingKeys: String, CodingKey {
-            case raw
-            case full
-            case regular
-            case small
-            case thumb
-            case smallS3 = "small_s3"
-        }
+    private enum CodingKeys: String, CodingKey {
+        case downloadLocation = "download_location"
     }
+}
+
+// MARK: - User
+struct User: ResponseDTO {
+    let id: String
+    let username: String
+    let name: String
+    let portfolioURL: String?
+    let bio: String?
+    let links: UserLinks
+    let profileImage: ProfileImage
+    let totalPhotos: Int
+    let totalIllustrations: Int
+    let social: Social
     
-    // MARK: - Links
-    struct Links: ResponseDTO {
-        let downloadLocation: String
-        
-        private enum CodingKeys: String, CodingKey {
-            case downloadLocation = "download_location"
-        }
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case username
+        case name
+        case portfolioURL = "portfolio_url"
+        case bio
+        case links
+        case profileImage = "profile_image"
+        case totalPhotos = "total_photos"
+        case totalIllustrations = "total_illustrations"
+        case social
     }
+}
+
+// MARK: - UserLinks
+struct UserLinks: ResponseDTO {
+    let linksSelf: String
+    let html: String
+    let photos: String
+    let likes: String
+    let portfolio: String
+    let following: String
+    let followers: String
     
-    // MARK: - User
-    struct User: ResponseDTO {
-        let id: String
-        let username: String
-        let name: String
-        let portfolioURL: String?
-        let bio: String?
-        let links: UserLinks
-        let profileImage: ProfileImage
-        let totalPhotos: Int
-        let totalIllustrations: Int
-        let social: Social
-        
-        private enum CodingKeys: String, CodingKey {
-            case id
-            case username
-            case name
-            case portfolioURL = "portfolio_url"
-            case bio
-            case links
-            case profileImage = "profile_image"
-            case totalPhotos = "total_photos"
-            case totalIllustrations = "total_illustrations"
-            case social
-        }
+    private enum CodingKeys: String, CodingKey {
+        case linksSelf = "self"
+        case html
+        case photos
+        case likes
+        case portfolio
+        case following
+        case followers
     }
+}
+
+// MARK: - ProfileImage
+struct ProfileImage: ResponseDTO {
+    let small: String
+    let medium: String
+    let large: String
+}
+
+// MARK: - Social
+struct Social: ResponseDTO {
+    let instagramUsername: String?
+    let twitterUsername: String?
     
-    // MARK: - UserLinks
-    struct UserLinks: ResponseDTO {
-        let linksSelf: String
-        let html: String
-        let photos: String
-        let likes: String
-        let portfolio: String
-        let following: String
-        let followers: String
-        
-        private enum CodingKeys: String, CodingKey {
-            case linksSelf = "self"
-            case html
-            case photos
-            case likes
-            case portfolio
-            case following
-            case followers
-        }
+    private enum CodingKeys: String, CodingKey {
+        case instagramUsername = "instagram_username"
+        case twitterUsername = "twitter_username"
     }
-    
-    // MARK: - ProfileImage
-    struct ProfileImage: ResponseDTO {
-        let small: String
-        let medium: String
-        let large: String
-    }
-    
-    // MARK: - Social
-    struct Social: ResponseDTO {
-        let instagramUsername: String?
-        let twitterUsername: String?
-        
-        private enum CodingKeys: String, CodingKey {
-            case instagramUsername = "instagram_username"
-            case twitterUsername = "twitter_username"
-        }
+}
+
+extension PhotoResponseDTO {
+    static func toModel() -> PhotoResponseDTO {
+        .init(id: "",
+              alternativeSlugs: AlternativeSlug(ko: ""),
+              createdAt: "",
+              width: 0,
+              height: 0,
+              color: "",
+              blurHash: "",
+              description: "",
+              altDescription: "",
+              photoUrls: PhotoUrls(raw: "", full: "", regular: "", small: "", thumb: "", smallS3: ""),
+              links: Links(downloadLocation: ""),
+              likes: 0,
+              assetType: .photo,
+              user: User(id: "",
+                         username: "",
+                         name: "",
+                         portfolioURL: "",
+                         bio: "",
+                         links: UserLinks(linksSelf: "",
+                                          html: "",
+                                          photos: "",
+                                          likes: "",
+                                          portfolio: "",
+                                          following: "",
+                                          followers: ""),
+                         profileImage: ProfileImage(small: "", medium: "", large: ""),
+                         totalPhotos: 0,
+                         totalIllustrations: 0,
+                         social: Social(instagramUsername: "", twitterUsername: ""))
+        )
     }
 }
