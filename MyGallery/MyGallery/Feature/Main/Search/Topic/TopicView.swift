f7d9a921be39c06b63a977c9d560243d452099a9
Nova <woohyeon3382@naver.com>
2024-08-07 21:05:15 +0900

    refactor: TopicView 파일 분리
    SearchView 에서 TopicView 를 제거하고 파일 분리

---
+++ MyGallery/MyGallery/Feature/Main/Search/Topic/TopicView.swift
@@ -0,0 +1,65 @@
+//
+//  TopicView.swift
+//  MyGallery
+//
+//  Created by Nova on 8/7/24.
+//
+
+import SwiftUI
+
+struct TopicView: View {
+    @EnvironmentObject private var pathModel: Path
+    @EnvironmentObject private var searchViewModel: SearchViewModel
+    
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            HStack {
+                Text("Topics")
+                    .font(.system(size: 25, weight: .bold))
+                    .foregroundStyle(.white)
+                
+                Spacer()
+            } //: HStack
+            .padding(.horizontal)
+            
+            LazyVGrid(columns: searchViewModel.topicsColumns, spacing: 10) {
+                ForEach(searchViewModel.topicList, id: \.id) { topic in
+                    Button(action: {
+                        self.pathModel.paths.append(.topicPhotoView(topicTitle: topic.title))
+                        self.searchViewModel.setTopicId(id: topic.id)
+                        self.searchViewModel.getTopicPhotoList()
+                    }, label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .foregroundStyle(.black)
+                                .overlay {
+                                    AsyncImage(url: URL(string: topic.coverPhoto.urls.regular)) { image in
+                                        image
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 180, height: 150)
+                                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                                            .clipped()
+                                            .opacity(0.7)
+                                    } placeholder: {
+                                        CustomProgressView()
+                                    } //: AsyncImage
+                                }
+                            
+                            Text(topic.title)
+                                .font(.system(size: 15, weight: .semibold))
+                                .foregroundStyle(.white)
+                        } //: ZStack
+                        .frame(width: 180, height: 150)
+                    }) //: Button
+                } //: ForEach
+            } //: LazyVGrid
+            .padding(.bottom, 20)
+        } //: ScrollView
+        .frame(maxWidth: 370)
+    }
+}
+
+#Preview {
+    TopicView()
+}
