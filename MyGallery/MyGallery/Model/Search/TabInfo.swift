685201b091e34148919bf231fb8f97da672bd245
Nova <woohyeon3382@naver.com>
2024-08-15 17:55:23 +0900

    feat: TabInfo 타입 구현

---
+++ MyGallery/MyGallery/Model/Search/TabInfo.swift
@@ -0,0 +1,12 @@
+//
+//  TabInfo.swift
+//  MyGallery
+//
+//  Created by Nova on 8/15/24.
+//
+
+enum TabInfo: String, CaseIterable {
+    case photo = "사진"
+    case user = "유저"
+    case collection = "컬렉션"
+}
